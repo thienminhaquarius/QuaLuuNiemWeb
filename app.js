1b642ffc2f476dd01f96cbe90aa9bb1294a25b3b
thienminhaquarius <32061193+thienminhaquarius@users.noreply.github.com>
2017-10-18 19:15:26 +0700

    1.01
    Chay roi

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
+var router=require('./server/router');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views/pages'));
 app.set('view engine', 'ejs');
+
+router(app);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 
 
 // catch 404 and forward to error handler
@@ -44,3 +44,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+app.set('port',process.env.PORT||3000);
+var server=app.listen(app.get('port'),function(){
+	console.log('Server running...');
+});
