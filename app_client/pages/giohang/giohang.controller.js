edeaf95c22ad6f2e5bff7076d9bfa46d479321b1
thienminhaquarius <32061193+thienminhaquarius@users.noreply.github.com>
2018-01-01 22:20:17 +0700

    kk

--- app_client/pages/giohang/giohang.controller.js
+++ app_client/pages/giohang/giohang.controller.js
@@ -5,18 +5,38 @@
 		var vm=this;
 		//san pham
 		vm.isLoggedIn=authentication.isLoggedIn();
-		var data1=JSON.stringify(["5a33a913ceb70510d49c9673","5a33a8cbceb70510d49c9672" ,"5a33a894ceb70510d49c9671"]);
-		var data={arrayHang:data1};
-		dataCRUD.xemGioHang(data)
-		.then(function(respone){
-			vm.sanphams=respone.data;
-			//alert('den day1');
-		},function(err){
-			//alert('den day2');
-			console.log(err);
-		});
+		
+		if(!vm.isLoggedIn){	// Chưa đăng nhập, xử dụng fake user
+			
+			var data1=JSON.stringify($rootScope.fakeUser.gioHang);
+			var data={arrayHang:data1};
+			dataCRUD.xemGioHang(data)
+			.then(function(respone){
+				vm.sanphams=respone.data;
+				//alert('den day1');
+			},function(err){
+				//alert('den day2');
+				console.log(err);
+			});
+		}else{// đã đăng nhập
+			var user=authentication.currentUser();
+			dataCRUD.gioHangByUserEmail(user.email)
+			.then(function(respone){
+				var data1=JSON.stringify(respone.data.gioHang);
+				var data={arrayHang:data1};
+				dataCRUD.xemGioHang(data)
+				.then(function(respone){
+					vm.sanphams=respone.data;
+					//alert('den day1');
+				},function(err){
+					//alert('den day2');
+					console.log(err);
+				});
 
-	
+			},function(err){
+				console.log(err);
+			});
+		}
 		
 
 		//thong tin giao hang
@@ -28,7 +48,7 @@
 			hinhThucThanhToan:'',
 			message:''
 		}
-		
+
 		if(vm.isLoggedIn){
 			var user=authentication.currentUser();
 			vm.formData.name=user.name;
